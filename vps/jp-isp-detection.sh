c73d10175531aa2484b4e523eeffbd36a7aca13a
‭ <>
2025-04-01 21:35:32 +0800

    ‭

--- vps/jp-isp-detection.sh
+++ vps/jp-isp-detection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# author: https://www.nodeseek.com/post-156563-1
+# 原帖: https://www.nodeseek.com/post-156563-1
 
 myipv4=$(curl -fsL ip.sb -4)
 myipv6=$(curl -fsL ip.sb -6)
-minsokukey=$(curl -fsL "https://minsoku.net/provider_detections" | grep gon.provider_detection_key | awk -F ';' '{print $7}' | awk -F '\"' '{print $2}')
+minsokukey=$(curl -fsL "https://minsoku.net/provider_detections" | grep 'gon.provider_detection_key' | sed -E 's/.*gon.provider_detection_key="([^"]+)".*/\1/')
 curl -fsL "https://api.minsoku.net/api/utils/provider_detection?ipv4_ip=${myipv4}&ipv6_ip=${myipv6}&provider_detection_key=${minsokukey}"
